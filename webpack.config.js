58d78b88691ed951b191198a93a7c2046b5124fe
Juho Vepsalainen <bebraw@gmail.com>
2016-04-01 13:43:56 +0300

    Step 7

--- webpack.config.js
+++ webpack.config.js
@@ -88,5 +88,13 @@ if(TARGET === 'start' || !TARGET) {
 }
 
 if(TARGET === 'build') {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
