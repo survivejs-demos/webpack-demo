91dd74a7902f8cb9bb2845f55315ddebbdbe16c9
Juho Vepsalainen <bebraw@gmail.com>
2016-04-01 12:50:00 +0300

    Step 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# webpack-demo - Webpack demo for "SurviveJS - Webpack"
+
+See [SurviveJS - Webpack](http://survivejs.com/webpack_react/introduction/) to learn how/why this configuration works.
+
+## License
+
+MIT.
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
