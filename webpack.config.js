83453751510ef08b3f4e5b91628fad9184adb363
Juho Vepsalainen <bebraw@gmail.com>
2016-04-12 09:30:53 +0300

    Push tasks to a file of their own
    Now main configuration file is a on a higher level. The lib file could
    be pushed to a npm package for example.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const validate = require('webpack-validator');
 
+const tasks = require('./lib/tasks');
+
 // Load *package.json* so we can use `dependencies` from there
 const pkg = require('./package.json');
 
@@ -51,17 +53,17 @@ switch(process.env.npm_lifecycle_event) {
           chunkFilename: '[chunkhash].js'
         }
       },
-      clean(PATHS.build),
-      setEnvironment({
+      tasks.clean(PATHS.build),
+      tasks.setEnvironment({
         key: 'process.env.NODE_ENV',
         value: 'production'
       }),
-      extractBundle({
+      tasks.extractBundle({
         name: 'vendor',
         entries: Object.keys(pkg.dependencies)
       }),
-      extractCSS(PATHS.app),
-      minify()
+      tasks.extractCSS(PATHS.app),
+      tasks.minify()
     );
   case 'start':
   default:
@@ -76,13 +78,13 @@ switch(process.env.npm_lifecycle_event) {
           })
         ]
       },
-      devServer({
+      tasks.devServer({
         // Customize host/port here if needed
         host: process.env.HOST,
         port: process.env.PORT
       }),
-      setupCSS(PATHS.app),
-      dontParse({
+      tasks.setupCSS(PATHS.app),
+      tasks.dontParse({
         name: 'react',
         path: PATHS.react
       })
@@ -90,127 +92,3 @@ switch(process.env.npm_lifecycle_event) {
 }
 
 module.exports = validate(config);
-
-function setupCSS(path) {
-  return {
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: path
-        }
-      ]
-    }
-  };
-}
-
-function devServer(options) {
-  return {
-    devServer: {
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
-
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Vagrant or Cloud9, set
-      // host: process.env.HOST || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default localhost
-      host: options.host,
-      port: options.port
-    },
-  };
-}
-
-function dontParse(options) {
-  const alias = {};
-  alias[options.name] = options.path;
-
-  return {
-    module: {
-      noParse: [
-        options.path
-      ]
-    },
-    resolve: {
-      alias: alias
-    }
-  };
-}
-
-function setEnvironment(options) {
-  const env = {};
-  env[options.key] = JSON.stringify(options.value);
-
-  return {
-    plugins: [
-      new webpack.DefinePlugin(env)
-    ]
-  };
-}
-
-function extractCSS(path) {
-  return {
-    module: {
-      loaders: [
-        // Extract CSS during build
-        {
-          test: /\.css$/,
-          loader: ExtractTextPlugin.extract('style', 'css'),
-          include: path
-        }
-      ]
-    },
-    plugins: [
-      // Output extracted CSS to a file
-      new ExtractTextPlugin('[name].[chunkhash].css')
-    ]
-  };
-}
-
-function extractBundle(options) {
-  const entry = {};
-  entry[options.name] = options.entries;
-
-  return {
-    // Define vendor entry point needed for splitting.
-    entry: entry,
-    plugins: [
-      // Extract bundle and manifest files. Manifest is
-      // needed for reliable caching.
-      new webpack.optimize.CommonsChunkPlugin({
-        names: [options.name, 'manifest']
-      })
-    ]
-  };
-}
-
-function clean(path) {
-  return {
-    plugins: [
-      new CleanWebpackPlugin([path])
-    ]
-  };
-}
-
-function minify() {
-  return {
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
-    ]
-  };
-}
