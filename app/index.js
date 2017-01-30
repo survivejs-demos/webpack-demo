b274055486887b37e04701dd32f8b1e07b42d936
Juho Vepsalainen <bebraw@gmail.com>
2017-01-30 14:44:09 +0200

    Simplify HMR setup

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ document.body.appendChild(demoComponent);
 if(module.hot) {
   // Capture hot update
   module.hot.accept('./component', () => {
-    const nextComponent = require('./component').component();
+    const nextComponent = component();
 
     // Replace old content with the hot loaded one
     document.body.replaceChild(nextComponent, demoComponent);
