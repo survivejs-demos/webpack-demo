3f19e56c307ef09c5a81bc9a4b2ac9ed9735eeb3
Juho Vepsalainen <bebraw@gmail.com>
2017-02-10 14:04:17 +0200

    Disable SVG bits from the build

--- webpack.parts.js
+++ webpack.parts.js
@@ -162,8 +162,8 @@ exports.loadFonts = function({ include, exclude, options } = {}) {
     module: {
       rules: [
         {
-          // Capture eot, ttf, svg, woff, and woff2
-          test: /\.(woff2?|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          // Capture eot, ttf, woff, and woff2
+          test: /\.(woff2?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
           include,
           exclude,
 
@@ -171,6 +171,14 @@ exports.loadFonts = function({ include, exclude, options } = {}) {
             loader: 'file-loader',
             options,
           },
+        },
+        {
+          // Ignore svg since it's too big
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          include,
+          exclude,
+
+          use: 'null-loader',
         },
       ],
     },
