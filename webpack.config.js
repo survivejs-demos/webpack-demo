c20661898b886687486370fca091949a5165d7d0
Juho Vepsalainen <bebraw@gmail.com>
2017-02-10 09:40:28 +0200

    Refactor config to a flatter format
    This becomes important as the config grows.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-const common = merge([
+const commonConfig = merge([
   {
     output: {
       path: PATHS.build,
@@ -35,141 +35,129 @@ const common = merge([
   parts.loadJavaScript({ include: PATHS.app }),
 ]);
 
-function production() {
-  return merge([
-    common,
-    {
-      performance: {
-        hints: 'warning', // 'error' or false are valid too
-        maxEntrypointSize: 100000, // in bytes
-        maxAssetSize: 50000, // in bytes
-      },
-      output: {
-        chunkFilename: 'scripts/[chunkhash].js',
-        filename: '[name].[chunkhash:8].js',
-      },
-      plugins: [
-        new webpack.HashedModuleIdsPlugin(),
-      ],
-      recordsPath: 'records.json',
+const productionConfig = merge([
+  {
+    performance: {
+      hints: 'warning', // 'error' or false are valid too
+      maxEntrypointSize: 100000, // in bytes
+      maxAssetSize: 50000, // in bytes
     },
-    parts.clean(PATHS.build),
-    parts.minifyJavaScript({ useSourceMap: true }),
-    parts.minifyCSS({
-      options: {
-        discardComments: {
-          removeAll: true,
-        },
-      },
-    }),
-    parts.attachRevision(),
-    parts.extractBundles({
-      bundles: [
-        {
-          name: 'vendor',
-          entries: ['react'],
-        },
-        {
-          name: 'manifest',
-        },
-      ],
-    }),
-    parts.generateSourceMaps({ type: 'source-map' }),
-    parts.lintJavaScript({ include: PATHS.app }),
-    parts.extractCSS({
-      use: ['css-loader', parts.autoprefix()],
-    }),
-    parts.purifyCSS({
-      paths: glob.sync(path.join(PATHS.app, '**', '*')),
-    }),
-    parts.setFreeVariable(
-      'process.env.NODE_ENV',
-      'production'
-    ),
-  ]);
-}
-
-function development() {
-  return merge([
-    common,
-    {
-      output: {
-        devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
-      },
-      plugins: [
-        new webpack.NamedModulesPlugin(),
-      ],
-    },
-    parts.generateSourceMaps({ type: 'cheap-module-eval-source-map' }),
-    parts.devServer({
-      // Customize host/port here if needed
-      host: process.env.HOST,
-      port: process.env.PORT,
-    }),
-    parts.lintJavaScript({
-      include: PATHS.app,
-      options: {
-        // Emit warnings over errors to avoid crashing
-        // HMR on error.
-        emitWarning: true,
-      },
-    }),
-    parts.loadCSS(),
-  ]);
-}
-
-function app() {
-  return {
-    entry: {
-      app: PATHS.app,
+    output: {
+      chunkFilename: 'scripts/[chunkhash].js',
+      filename: '[name].[chunkhash:8].js',
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Webpack demo',
-      }),
+      new webpack.HashedModuleIdsPlugin(),
     ],
-  };
-}
+    recordsPath: 'records.json',
+  },
+  parts.clean(PATHS.build),
+  parts.minifyJavaScript({ useSourceMap: true }),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+    },
+  }),
+  parts.attachRevision(),
+  parts.extractBundles({
+    bundles: [
+      {
+        name: 'vendor',
+        entries: ['react'],
+      },
+      {
+        name: 'manifest',
+      },
+    ],
+  }),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  parts.lintJavaScript({ include: PATHS.app }),
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+  parts.purifyCSS({
+    paths: glob.sync(path.join(PATHS.app, '**', '*')),
+  }),
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    'production'
+  ),
+]);
 
-function react() {
-  return {
-    entry: {
-      react: PATHS.reactDemo,
+const developmentConfig = merge([
+  {
+    output: {
+      devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: HtmlWebpackTemplate,
-        title: 'React demo',
-        filename: 'react/index.html',
-        appMountId: 'app', // Generate #app where to mount
-        mobile: true, // Scale page on mobile
-        inject: false, // html-webpack-template needs this to work
-      }),
+      new webpack.NamedModulesPlugin(),
     ],
-  };
-}
-
-function reactDevelopment() {
-  return {
-    entry: {
-      // react-hot-loader has to run before demo!
-      react: ['react-hot-loader/patch', PATHS.reactDemo],
+  },
+  parts.generateSourceMaps({ type: 'cheap-module-eval-source-map' }),
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+  parts.lintJavaScript({
+    include: PATHS.app,
+    options: {
+      // Emit warnings over errors to avoid crashing
+      // HMR on error.
+      emitWarning: true,
     },
-  };
-}
+  }),
+  parts.loadCSS(),
+]);
+
+const appConfig = {
+  entry: {
+    app: PATHS.app,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo',
+    }),
+  ],
+};
+
+const reactConfig = {
+  entry: {
+    react: PATHS.reactDemo,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: HtmlWebpackTemplate,
+      title: 'React demo',
+      filename: 'react/index.html',
+      appMountId: 'app', // Generate #app where to mount
+      mobile: true, // Scale page on mobile
+      inject: false, // html-webpack-template needs this to work
+    }),
+  ],
+};
+
+const reactDevelopmentConfig = {
+  entry: {
+    // react-hot-loader has to run before demo!
+    react: ['react-hot-loader/patch', PATHS.reactDemo],
+  },
+};
 
 module.exports = function(env) {
   process.env.BABEL_ENV = env;
 
   if (env === 'production') {
     return [
-      merge(production(), app()),
-      merge(production(), react()),
+      merge(commonConfig, productionConfig, appConfig),
+      merge(commonConfig, productionConfig, reactConfig),
     ];
   }
 
   return [
-    merge(development(), app()),
-    merge(development(), react(), reactDevelopment()),
+    merge(commonConfig, developmentConfig, appConfig),
+    merge(commonConfig, developmentConfig, reactConfig, reactDevelopmentConfig),
   ];
 };
