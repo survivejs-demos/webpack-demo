02db8ef9235a557f222f9a225bda20064daccead
Juho Vepsalainen <bebraw@gmail.com>
2017-02-13 14:09:01 +0200

    Add lib setup

---
+++ webpack.lib.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  lib: path.join(__dirname, 'lib'),
+  build: path.join(__dirname, 'dist'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      lib: PATHS.lib,
+    },
+    output: {
+      path: PATHS.build,
+      library: 'Demo',
+      libraryTarget: 'var',
+    },
+  },
+  parts.attachRevision(),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  parts.loadJavaScript({ include: PATHS.app }),
+]);
+
+const libraryConfig = merge([
+  commonConfig,
+  {
+    output: {
+      filename: '[name].js',
+    },
+  },
+]);
+
+const libraryMinConfig = merge([
+  commonConfig,
+  {
+    output: {
+      filename: '[name].min.js',
+    },
+  },
+  parts.minifyJavaScript({ useSourceMap: true }),
+]);
+
+module.exports = [
+  libraryConfig,
+  libraryMinConfig,
+];
