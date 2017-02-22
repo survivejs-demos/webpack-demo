dcd73cd570ff43ceb49e5bcc695949fa4f17f159
Juho Vepsalainen <bebraw@gmail.com>
2017-02-22 13:55:13 +0200

    Simplify further

--- webpack.mocha.js
+++ webpack.mocha.js
@@ -3,16 +3,12 @@ const merge = require('webpack-merge');
 
 const parts = require('./webpack.parts');
 
-const PATHS = {
-  tests: path.join(__dirname, 'tests'),
-};
-
 module.exports = merge([
   parts.devServer(),
   parts.page({
     title: 'Mocha demo',
     entry: {
-      tests: PATHS.tests,
+      tests: path.join(__dirname, 'tests'),
     },
   }),
 ]);
