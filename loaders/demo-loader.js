a73c2b643914fe7aaba9961fae730b3647feb620
Juho Vepsalainen <bebraw@gmail.com>
2017-03-11 14:59:57 +0200

    Simplify definitions

--- loaders/demo-loader.js
+++ loaders/demo-loader.js
@@ -1,11 +1,11 @@
 const loaderUtils = require('loader-utils');
 
-module.exports = function(input) {
+module.exports = (input) => {
   const { text } = loaderUtils.getOptions(this);
 
   return input + text;
 };
-module.exports.pitch = function(remainingRequest, precedingRequest, input) {
+module.exports.pitch = (remainingRequest, precedingRequest, input) => {
   console.log(
     'remaining request', remainingRequest,
     'preceding request', precedingRequest,
