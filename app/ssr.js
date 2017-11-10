74549e90e1f37b0a40709612425961b93839a074
Juho Vepsalainen <bebraw@gmail.com>
2017-11-10 10:27:03 +0100

    fix(ssr): hydrate over render

--- app/ssr.js
+++ app/ssr.js
@@ -7,5 +7,5 @@ const SSR = <div onClick={() => alert("hello")}>Hello world</div>;
 if (typeof document === "undefined") {
   module.exports = SSR;
 } else {
-  ReactDOM.render(SSR, document.getElementById("app"));
+  ReactDOM.hydrate(SSR, document.getElementById("app"));
 }
