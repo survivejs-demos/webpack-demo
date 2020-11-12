2a3d3c823adf89d93094284f32f49459cf063aa8
Juho Vepsäläinen <bebraw@gmail.com>
2020-11-12 13:04:21 +0100

    chore: Decouple ssr config

--- webpack.ssr.js
+++ webpack.ssr.js
@@ -1,19 +1,23 @@
 const path = require("path");
-const { merge } = require("webpack-merge");
-const parts = require("./webpack.parts");
 
-module.exports = merge([
-  {
-    mode: "production",
-    entry: {
-      index: path.join(__dirname, "src", "ssr.js"),
-    },
-    output: {
-      path: path.join(__dirname, "static"),
-      filename: "[name].js",
-      libraryTarget: "umd",
-      globalObject: "this",
-    },
+const APP_SOURCE = path.join(__dirname, "src");
+
+module.exports = {
+  mode: "production",
+  entry: { index: path.join(APP_SOURCE, "ssr.js") },
+  output: {
+    path: path.join(__dirname, "static"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    globalObject: "this",
   },
-  parts.loadJavaScript(),
-]);
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: APP_SOURCE,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
