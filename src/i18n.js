995cf157661d0866ab1def37789c606cc7de76d8
Juho Vepsäläinen <bebraw@gmail.com>
2020-11-23 11:45:11 +0100

    chore: Update i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,1 +1,33 @@
-console.log(__("greeting.hello"));
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [language, setLanguage] = useState("en");
+  const [hello, setHello] = useState("");
+  const changeLanguage = () => setLanguage(language === "en" ? "fi" : "en");
+
+  useEffect(() => {
+    translate(language, "hello").then(setHello).catch(console.error);
+  }, [language]);
+
+  return (
+    <div>
+      <button onClick={changeLanguage}>Change language</button>
+      <div>{hello}</div>
+    </div>
+  );
+};
+
+function translate(locale, text) {
+  return getLocaleData(locale).then((messages) => messages[text]);
+}
+
+async function getLocaleData(locale) {
+  return import(`../messages/${locale}.json`);
+}
+
+const root = document.createElement("div");
+root.setAttribute("id", "app");
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
--- webpack.i18n.js
+++ webpack.i18n.js
@@ -1,29 +1,18 @@
 const path = require("path");
-const I18nPlugin = require("embed-i18n-webpack-plugin");
+const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
+const APP_SOURCE = path.join(__dirname, "src");
 
-const TRANSLATIONS = {
-  fi: {
-    greeting: {
-      hello: "Terve maailma",
-    },
+module.exports = {
+  mode: "production",
+  entry: { index: path.join(APP_SOURCE, "i18n.js") },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: APP_SOURCE,
+        use: "babel-loader",
+      },
+    ],
   },
-  en: {
-    greeting: {
-      hello: "Hello world",
-    },
-  },
+  plugins: [new MiniHtmlWebpackPlugin()],
 };
-
-module.exports = Object.entries(TRANSLATIONS).map(
-  ([language, translation]) => ({
-    mode: "production",
-    entry: {
-      index: path.join(__dirname, "src", "i18n.js"),
-    },
-    output: {
-      path: path.join(__dirname, "i18n-build"),
-      filename: `[name].${language}.js`,
-    },
-    plugins: [new I18nPlugin(translation)],
-  })
-);
